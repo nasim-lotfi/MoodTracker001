a93c810901cf3f5ca97c79ab71fc2c89e33e674d
nasim lotfi <nassimlotfi7618@gmail.com>
2024-12-18 14:18:36 +0100

    Initial Commit

---
+++ MoodTracker/MoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodTrackerApp.swift
+//  MoodTracker
+//
+//  Created by nasim on 18/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoodTrackerTests/MoodTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoodTrackerTests.swift
+//  MoodTrackerTests
+//
+//  Created by nasim on 18/12/24.
+//
+
+import Testing
+@testable import MoodTracker
+
+struct MoodTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
